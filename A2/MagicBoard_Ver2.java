ebe8c25e3c0f52d838932d6785c3d78f684b8a7a
chelsieng <chelsiengmk@gmail.com>
2020-10-18 16:38:53 -0400

    Test log file version2 done

--- A2/MagicBoard_Ver2.java
+++ A2/MagicBoard_Ver2.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class MagicBoard_Ver2 {
@@ -16,7 +19,6 @@ public class MagicBoard_Ver2 {
             int goDown = down(p.first, p.second, board); // Go south
             int goRight = right(p.first, p.second, board); // Go east
             int goLeft = left(p.first, p.second, board); // Go west
-
             // Goal is reached
             if (board[p.first][p.second] == 0) {
                 return true;
@@ -80,109 +82,122 @@ public class MagicBoard_Ver2 {
     }
 
     public static void main(String[] args) {
-        // Welcome Message
-        System.out.println(" ------------------------------ ");
-        System.out.println("| Welcome to Magic Board Game! |");
-        System.out.println(" ------------------------------ ");
+        try {
+            PrintWriter writer = new PrintWriter(new FileOutputStream("A2/testLog_ver2.txt", true));
+            // Welcome Message
+            System.out.println(" ------------------------------ ");
+            System.out.println("| Welcome to Magic Board Game! |");
+            System.out.println(" ------------------------------ ");
 
-        // Prompting user for size of board
-        Scanner userIn = new Scanner(System.in);
-        System.out.println("Please enter the size of your board: ");
-        int size = userIn.nextInt();
-        // If size entered by user < 5, keep prompting
-        while (size < 5){
-            System.out.println("Too small, try again!");
-            size = userIn.nextInt();
-        }
-        // Creating size x size array
-        int [][] board = new int[size][size];
-        Random random = new Random();
-        int randZero1 = random.nextInt(size);
-        int randZero2 = random.nextInt(size);
-        int randNum = random.nextInt(size-1) + 1;
-
-        // Randomly placing numbers in array
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                board[i][j] = randNum;
-                randNum = random.nextInt(size-1)+1;
+            // Prompting user for size of board
+            Scanner userIn = new Scanner(System.in);
+            System.out.println("Please enter the size of your board: ");
+            int size = userIn.nextInt();
+            // If size entered by user < 5, keep prompting
+            while (size < 5) {
+                System.out.println("Too small, try again!");
+                size = userIn.nextInt();
             }
-        }
-//        // Randomly placing 0 value in array
-        board[randZero1][randZero2] = 0;
+            // Creating size x size array
+            int[][] board = new int[size][size];
+            Random random = new Random();
+            int randZero1 = random.nextInt(size);
+            int randZero2 = random.nextInt(size);
+            int randNum = random.nextInt(size - 1) + 1;
 
-        //Uncomment for custom board (not possible to solve)
+            // Randomly placing numbers in array
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    board[i][j] = randNum;
+                    randNum = random.nextInt(size - 1) + 1;
+                }
+            }
+//        // Randomly placing 0 value in array
+            board[randZero1][randZero2] = 0;
+
+            //Uncomment for custom board (not possible to solve)
 //        int [][]board= {{1,4,1,3,1},
 //                {4,3,2,1,4},
 //                {3,2,3,1,4},
 //                {1,3,4,2,3},
 //                {3,4,1,2,0}};
 
-        // Uncomment for custom board (possible to solve)
+            // Uncomment for custom board (possible to solve)
 //        int[][] board = {{4, 2, 1, 3, 1},
 //                {2, 3, 2, 1, 4},
 //                {3, 2, 3, 1, 4},
 //                {1, 3, 4, 2, 3},
 //                {3, 3, 1, 2, 0}};
 
-        //Displaying board
-        System.out.println("Your board looks like this: ");
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j] + "\t");
+            //Displaying board
+            System.out.println("Your board looks like this: ");
+            writer.println(" ------------------- ");
+            writer.println("| Board size: " + board.length + " x " + board.length + " |");
+            writer.println(" ------------------- ");
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    System.out.print(board[i][j] + "\t");
+                    writer.print(board[i][j] + "\t");
+                }
+                System.out.println();
+                writer.println();
             }
-            System.out.println();
-        }
-        boolean illegal = true;
-        int x = 999;
-        int y = 999;
-        // Keep prompting user to choose start position if position is illegal
-        while (illegal) {
-            // Prompting user to select start position
-            System.out.println("\nChoose your legal start position (x,y): ");
-            System.out.println("\t1. (0, 0)");
-            System.out.println("\t2. (0, " + (board.length - 1) + ")");
-            System.out.println("\t3. (" + (board.length - 1) + ", 0)");
-            System.out.println("\t4. (" + (board.length - 1) + ", " + (board.length - 1) + ")");
-            System.out.println(">>");
-            int startAt = userIn.nextInt();
-            // Keeping prompting user to select 1-4
-            while (startAt <= 0 || startAt > 4) {
+            boolean illegal = true;
+            int x = 999;
+            int y = 999;
+            // Keep prompting user to choose start position if position is illegal
+            while (illegal) {
+                // Prompting user to select start position
                 System.out.println("\nChoose your legal start position (x,y): ");
                 System.out.println("\t1. (0, 0)");
                 System.out.println("\t2. (0, " + (board.length - 1) + ")");
                 System.out.println("\t3. (" + (board.length - 1) + ", 0)");
                 System.out.println("\t4. (" + (board.length - 1) + ", " + (board.length - 1) + ")");
                 System.out.println(">>");
-                startAt = userIn.nextInt();
+                int startAt = userIn.nextInt();
+                // Keeping prompting user to select 1-4
+                while (startAt <= 0 || startAt > 4) {
+                    System.out.println("\nChoose your legal start position (x,y): ");
+                    System.out.println("\t1. (0, 0)");
+                    System.out.println("\t2. (0, " + (board.length - 1) + ")");
+                    System.out.println("\t3. (" + (board.length - 1) + ", 0)");
+                    System.out.println("\t4. (" + (board.length - 1) + ", " + (board.length - 1) + ")");
+                    System.out.println(">>");
+                    startAt = userIn.nextInt();
+                }
+                if (startAt == 1) {
+                    x = 0;
+                    y = 0;
+                } else if (startAt == 2) {
+                    x = 0;
+                    y = board.length - 1;
+                } else if (startAt == 3) {
+                    x = board.length - 1;
+                    y = 0;
+                } else if (startAt == 4) {
+                    x = board.length - 1;
+                    y = board.length - 1;
+                }
+                // Illegal start position at zero
+                if (board[x][y] == 0) {
+                    System.out.println("Illegal start! Zero is the goal, try again!");
+                    illegal = true;
+                } else {
+                    illegal = false; //Legal start position
+                }
             }
-            if (startAt == 1) {
-                x = 0;
-                y = 0;
-            } else if (startAt == 2) {
-                x = 0;
-                y = board.length - 1;
-            } else if (startAt == 3) {
-                x = board.length - 1;
-                y = 0;
-            } else if (startAt == 4) {
-                x = board.length - 1;
-                y = board.length - 1;
-            }
-            // Illegal start position at zero
-            if (board[x][y] == 0) {
-                System.out.println("Illegal start! Zero is the goal, try again!");
-                illegal = true;
+            writer.println("Start position: (" + x + ", " + y + ")");
+            if (MagicBoard(x, y, board)) {
+                System.out.println("It is possible to solve the game from start position.");
+                writer.println("It is possible to solve the game from start position.\n");
             } else {
-                illegal = false; //Legal start position
+                System.out.println("It is not possible to solve the game from start position.");
+                writer.println("It is not possible to solve the game from start position.\n");
             }
-        }
-        if (MagicBoard(x, y, board)) {
-            System.out.println("It is possible to solve the game from start position.");
-//            writer.println("It is possible to solve the game from start position.\n");
-        } else {
-            System.out.println("It is not possible to solve the game from start position.");
-//            writer.println("It is not possible to solve the game from start position.\n");
+            writer.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open/write to file. Program terminated.");
+            System.exit(-1);
         }
     }
 }
